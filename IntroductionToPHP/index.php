bf4fd5cc1598fa56d6f967d455f16678c207cdff
Galina <baleva1990@gmail.com>
2023-12-29 23:31:55 +0100

    PHP arrays

--- IntroductionToPHP/index.php
+++ IntroductionToPHP/index.php
@@ -3,10 +3,39 @@ $name = 'SoftUni';
 
 //echo name;
 
-echo "hello, $name!";
-echo '<br /';
-$name = $name . ' Edited';
-echo 'hello, ' .$name. '!';
+// echo "hello, $name!";
+// echo '<br /';
+// $name = $name . ' Edited';
+// echo 'hello, ' .$name. '!';
+// echo '<br />';
+// echo 'hello, ' . $name . '!';
+
+
+
+$x = 1;
+$y = 2;
+$sum = $x + $y;
+echo $sum;
 echo '<br />';
-echo 'hello, ' . $name . '!';
-?>
+$sum = $x+$y;
+echo $sum;
+
+
+
+var_dump( $x );
+var_dump( $sum );
+
+$color = array( 
+    'red', 
+    'green', 
+    'blue', 
+    'balck'
+    );
+var_dump( $color );
+
+$numbers = [ 'One', 'two', 'three' ];
+
+var_dump( $numbers );
+
+$numbers_with_keys = [ 99 => 'One', 1 => 'two', 2 => 'three' ];
+var_dump( $numbers_with_keys );

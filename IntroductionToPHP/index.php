0541ca83cf7b9f80bf58b88e4bdb494385f7709e
Galina <baleva1990@gmail.com>
2023-12-29 23:05:19 +0100

    First demo with php

---
+++ IntroductionToPHP/index.php
@@ -0,0 +1,12 @@
+<?php
+$name = 'SoftUni';
+
+//echo name;
+
+echo "hello, $name!";
+echo '<br /';
+$name = $name . ' Edited';
+echo 'hello, ' .$name. '!';
+echo '<br />';
+echo 'hello, ' . $name . '!';
+?>

0a7c27b3bf1fcfce60fbebb6da04b61411c8402e
Galina <baleva1990@gmail.com>
2024-01-17 10:44:08 +0700

    Adding dynamic in template

--- ThemeDevelopmentIntroduction/wp-content/themes/softuni/single.php
+++ ThemeDevelopmentIntroduction/wp-content/themes/softuni/single.php
@@ -1,46 +1,43 @@
 <?php get_header(); ?>
                    
-                        <div class="intro row">
-                            <div class="overlay"></div>
-                            <div class="col-sm-12">
-                                <ol class="breadcrumb">
-                                    <li><a href="index.html">Home</a></li>
-                                    <li class="active">FAQ</li>
-                                </ol>
-                            </div>
-                        </div> <!-- /.intro.row -->
-                    </div> <!-- /.container -->
-                    <div class="nutral"></div>
-                </section> <!-- /#header -->
+        <div class="intro row">
+            <div class="overlay"></div>
+            <div class="col-sm-12">
+                <ol class="breadcrumb">
+                    <li><a href="index.html">Home</a></li>
+                    <li class="active"><?php the_title(); ?></li>
+                </ol>
+            </div>
+        </div> <!-- /.intro.row -->
+    </div> <!-- /.container -->
+    <div class="nutral"></div>
+</section> <!-- /#header -->
 
-            <!-- FAQ -->
-                <section class="faq">
-                    <?php if (have_posts()) : ?>
-                        <?php while (have_posts()) : ?>
-                            <div class="container page-bgc">
-                     <div class="row">
-                            <div class="col-sm-12">
-                                <div class="title-box">
-                                    <h2 class="title mt0">Questions</h2>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-8 col-sm-offset-2">
-                                <p class="inner-p">
-                                    Lorem ipsum dolor sit amet event landing template, consectetur adipisicing elit. Suscipit corrupti facilis event landing template, enim earum numquam minus veritatis nobis accusamus similique, totam?
-                                </p>
-                            </div>
+<!-- FAQ -->
+<section class="faq">
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+        <div class="container page-bgc">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="title-box">
+                            <h2 class="title mt0"><?php the_title(); ?></h2>
                         </div>
                     </div>
-                        <?php endwhile; ?>
-
-                    <? else : ?>
-
-                        Sorry, there is nothing I can show you!
-
-                    <?php endif; ?>
-                </section>
+                </div>
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2">
+                        <p class="inner-p">
+                            <?php the_content(); ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php else : ?>
+        Sorry, there is nothing I can show you!
+    <?php endif; ?>
+</section>
 
 <?php get_footer(); ?>
 

9bc71444b568e74730907cd672f5945a64128785
Galina <baleva1990@gmail.com>
2024-03-20 13:21:12 +0100

    Pagination to the blog page is added.

--- Project Robots/themes/softuni/templates/blog.php
+++ Project Robots/themes/softuni/templates/blog.php
@@ -10,6 +10,7 @@ $blog_query_args = array(
     'post_type'     => 'post',
     'post_status'   => 'publish',
     'posts_per_page' => 9,
+    'paged' =>  get_query_var( 'paged' )
 );
 
 
@@ -37,12 +38,25 @@ $blog_query = new WP_Query($blog_query_args);
                 <?php while ($blog_query->have_posts()) : $blog_query->the_post(); ?>
 
                     <?php get_template_part('partials/content', 'blog') ?>
-                    
+
                 <?php endwhile; ?>
             </div>
         </div>
     </section>
 
+    <div style="text-align: center;">
+        <?php
+        $GLOBALS['wp_query'] = $blog_query;
+        the_posts_pagination(
+            array(
+                'mid_size'  => 1,
+                'prev_text' => __('Previos', 'robots'),
+                'next_text' => __('Next', 'robots'),
+                'text_align' => 'center'
+            )
+        );
+        ?>
+    </div>
 <?php endif; ?>
 
 <?php wp_reset_postdata(); ?>

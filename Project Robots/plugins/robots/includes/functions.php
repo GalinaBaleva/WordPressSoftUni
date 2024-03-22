fba9333219a4b493c4752e6a4ff43fe5ea8a38b0
Galina <baleva1990@gmail.com>
2024-03-22 12:12:45 +0100

    Adding 'shortcode' to get post id

--- Project Robots/plugins/robots/includes/functions.php
+++ Project Robots/plugins/robots/includes/functions.php
@@ -1,3 +1,45 @@
 <?php
 
 // place for general functions
+
+
+//Options for debuging
+// $var = array(
+//     1,
+//     2,
+//     3,
+//     4,
+//     5,
+//     6,
+// );
+
+// echo '<pre>';
+// var_dump($var);
+// echo '</pre>';
+
+// error_log( 'Custom message in the error log with variable' . $array );
+
+
+/**
+ * Custom shortcode to show the title by ID
+ */
+
+
+function show_post_by_id($atts)
+{
+    $shortcode_atts = shortcode_atts(
+        array(
+            'id' => '',
+        ),
+        $atts
+    );
+    $title = '';
+
+    if (!empty($shortcode_atts['id'])) {
+        $title = get_the_title($shortcode_atts['id']);
+    }
+
+    return $title;
+}
+
+add_shortcode('show_post_title', 'show_post_title_by_id');
